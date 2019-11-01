3cc094509a58e9b9e9016a83e468062953f6ece9
Frederico Apolónia <fredericojcapolo@gmail.com>
2019-11-01 23:28:53 +0000

    run() da thread praticamente terminado

--- src/Client/TrafficGeneratorPerformanceMonitoring.java
+++ src/Client/TrafficGeneratorPerformanceMonitoring.java
@@ -1,9 +1,24 @@
 package Client;
 
+import AllStoresServer.Interfaces.AllStoresServerInterface;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TrafficGeneratorPerformanceMonitoring {
+
+    private static final String ALLSTORES_HOST = "127.0.0.1";
+    private static final int ALLSTORES_PORT = 1099;
+    private static final String ALLSTORES_REGISTRY_NAME = "ClientServerInterface";
+
+    private static final int ALLSTORES_MAX_SHOPID = 600;
+    private static final int ALLSTORES_MAX_PRODUCTID = 20;
+    private static final int BUY_RESERVE_QUANTITY = 1;
 
     private static class Options {
         int numberClients;
@@ -16,6 +31,62 @@ public class TrafficGeneratorPerformanceMonitoring {
         Options clientOptions = readArguments(args);
         System.out.println(String.format("Number of clients: %d\nWrite mode = %b\nStoreID = %d\nTraffic time = %d",
                 clientOptions.numberClients, clientOptions.writeMode, clientOptions.storeID, clientOptions.trafficTime));
+
+        // ...
+        // todo inicializar X threads durante Y segundos (armar um alarme)
+    }
+
+    class ClientThead extends Thread {
+
+        private final boolean writeMode;
+        private final int singleStore;
+        private final int clientID;
+        private final AllStoresServerInterface allStoresServer;
+        private int actionCount = 0;
+        private int unavailableCount = 0;
+        private long timeSum;
+
+        ClientThead (boolean writeMode, int singleStore, int clientID) throws RemoteException, NotBoundException {
+            this.writeMode = writeMode;
+            this.singleStore = singleStore;
+            this.clientID = clientID;
+
+            Registry registry = LocateRegistry.getRegistry(ALLSTORES_HOST, ALLSTORES_PORT);
+            this.allStoresServer = (AllStoresServerInterface) registry.lookup(ALLSTORES_REGISTRY_NAME);
+        }
+
+        public void run() {
+            // todo falta ver como retornar valores quando o pai chama as threads de volta
+            int store = singleStore;
+            int product;
+            long start, end;
+            Random rnd = new Random();
+            try {
+                while (true) {
+                    product = rnd.nextInt(ALLSTORES_MAX_PRODUCTID) + 1;
+                    if (singleStore != -1) {
+                        // random store
+                        store = rnd.nextInt(ALLSTORES_MAX_SHOPID) + 1;
+                    }
+                    if (writeMode) {
+                        start = System.currentTimeMillis();
+                        Client.buy(this.allStoresServer, this.clientID, store, product, BUY_RESERVE_QUANTITY);
+                        end = System.currentTimeMillis();
+                    } else {
+                        start = System.currentTimeMillis();
+                        Client.reserve(this.allStoresServer, this.clientID, store, product, BUY_RESERVE_QUANTITY);
+                        end = System.currentTimeMillis();
+                    }
+                    // todo if busy => nao se conta
+                    // todo if unavailable => counterUnavailable
+                    actionCount++;
+                    timeSum += (end - start);
+                }
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
     /**
