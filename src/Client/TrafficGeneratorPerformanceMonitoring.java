850711382f8b84bd0b75fa06c614ee34652e5533
fc47067 <fc47067@1224-11>
2019-11-03 18:10:30 +0000

    alteracoes para fazer print dos resultados depois d etodas as threads terem acabado

--- src/Client/TrafficGeneratorPerformanceMonitoring.java
+++ src/Client/TrafficGeneratorPerformanceMonitoring.java
@@ -13,232 +13,263 @@ import java.util.regex.Pattern;
 
 public class TrafficGeneratorPerformanceMonitoring {
 
-    private static final String ALLSTORES_HOST = "127.0.0.1";
-    private static final int ALLSTORES_PORT = 1099;
-    private static final String ALLSTORES_REGISTRY_NAME = "ClientServerInterface";
+	private static final String ALLSTORES_HOST = "127.0.0.1";
+	private static final int ALLSTORES_PORT = 1099;
+	private static final String ALLSTORES_REGISTRY_NAME = "ClientServerInterface";
 
-    private static final int ALLSTORES_MAX_SHOPID = 600;
-    private static final int ALLSTORES_MAX_PRODUCTID = 20;
-    private static final int BUY_RESERVE_QUANTITY = 1;
+	private static final int ALLSTORES_MAX_SHOPID = 600;
+	private static final int ALLSTORES_MAX_PRODUCTID = 20;
+	private static final int BUY_RESERVE_QUANTITY = 1;
 
-    private static boolean stopThreads = false;
-    private static int elapsedTime;
+	private static boolean stopThreads = false;
+	private static int elapsedTime;
 
-    private static class Options {
-        int numberClients;
-        boolean writeMode = false;
-        int storeID = -1;
-        int trafficTime;
-    }
+	private static class Options {
+		int numberClients;
+		boolean writeMode = false;
+		int storeID = -1;
+		int trafficTime;
+	}
 
-    private class ThreadResult {
-        int actionCount = 0;
-        int replyCount = 0;
-        int unavailableCount = 0;
-        long timeSum;
-    }
+	private class ThreadResult extends Thread {
+		int actionCount = 0;
+		int replyCount = 0;
+		int unavailableCount = 0;
+		long timeSum;
+	}
 
-    class ClientThead extends Thread implements Callable<ThreadResult> {
+	class ClientThead extends Thread implements Callable<ThreadResult> {
 
-        private final boolean writeMode;
-        private final int singleStore;
-        private final int clientID;
-        private final AllStoresServerInterface allStoresServer;
-        private ThreadResult result = new ThreadResult();
+		private final boolean writeMode;
+		private final int singleStore;
+		private final int clientID;
+		private final AllStoresServerInterface allStoresServer;
+		private ThreadResult result = new ThreadResult();
 
-        ClientThead (boolean writeMode, int singleStore, int clientID) throws RemoteException, NotBoundException {
-            this.writeMode = writeMode;
-            this.singleStore = singleStore;
-            this.clientID = clientID;
+		ClientThead(boolean writeMode, int singleStore, int clientID) throws RemoteException, NotBoundException {
+			this.writeMode = writeMode;
+			this.singleStore = singleStore;
+			this.clientID = clientID;
 
-            Registry registry = LocateRegistry.getRegistry(ALLSTORES_HOST, ALLSTORES_PORT);
-            this.allStoresServer = (AllStoresServerInterface) registry.lookup(ALLSTORES_REGISTRY_NAME);
-        }
+			Registry registry = LocateRegistry.getRegistry(ALLSTORES_HOST, ALLSTORES_PORT);
+			this.allStoresServer = (AllStoresServerInterface) registry.lookup(ALLSTORES_REGISTRY_NAME);
+		}
 
-        public ThreadResult getResult() {
-            return result;
-        }
+		public ThreadResult getResult() {
+			return result;
+		}
 
-        public void run() {
-            int store = singleStore;
-            int product;
-            long start, end;
-            Random rnd = new Random();
-            String actionResult;
-            boolean randomStores = singleStore == -1;
-            try {
-                while (!stopThreads) {
-                    product = rnd.nextInt(ALLSTORES_MAX_PRODUCTID) + 1;
-                    if (randomStores) {
-                        // random store
-                        store = rnd.nextInt(ALLSTORES_MAX_SHOPID) + 1;
-                    }
-                    if (writeMode) {
-                        start = System.currentTimeMillis();
-                        actionResult = Client.buy(this.allStoresServer, this.clientID, store, product, BUY_RESERVE_QUANTITY);
-                        end = System.currentTimeMillis();
-                    } else {
-                        start = System.currentTimeMillis();
-                        actionResult = Client.reserve(this.allStoresServer, this.clientID, store, product, BUY_RESERVE_QUANTITY);
-                        end = System.currentTimeMillis();
-                    }
-                    this.result.actionCount++;
+		public void run() {
+			int store = singleStore;
+			int product;
+			long start, end;
+			Random rnd = new Random();
+			String actionResult;
+			boolean randomStores = singleStore == -1;
+			try {
+				while (!stopThreads) {
+					product = rnd.nextInt(ALLSTORES_MAX_PRODUCTID) + 1;
+					if (randomStores) {
+						// random store
+						store = rnd.nextInt(ALLSTORES_MAX_SHOPID) + 1;
+					}
+					if (writeMode) {
+						start = System.currentTimeMillis();
+						actionResult = Client.buy(this.allStoresServer, this.clientID, store, product,
+								BUY_RESERVE_QUANTITY);
+						end = System.currentTimeMillis();
+					} else {
+						start = System.currentTimeMillis();
+						actionResult = Client.reserve(this.allStoresServer, this.clientID, store, product,
+								BUY_RESERVE_QUANTITY);
+						end = System.currentTimeMillis();
+					}
+					this.result.actionCount++;
 
-                    if (!actionResult.toLowerCase().contains("busy") && !actionResult.toLowerCase().contains("error")) {
-                        this.result.replyCount++;
-                        this.result.timeSum += (end - start);
-                    }
-                    if (actionResult.toLowerCase().contains("unavailable")) {
-                        this.result.unavailableCount++;
-                    }
-                }
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            }
-        }
+					if (!actionResult.toLowerCase().contains("busy") && !actionResult.toLowerCase().contains("error")) {
+						this.result.replyCount++;
+						this.result.timeSum += (end - start);
+					}
+					if (actionResult.toLowerCase().contains("unavailable")) {
+						this.result.unavailableCount++;
+					}
+				}
 
-        @Override
-        public ThreadResult call() throws Exception {
-            return this.result;
-        }
-    }
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
+		}
 
-    public static void main(String[] args) {
-        Options options = readArguments(args);
-        System.out.println(String.format("Number of clients: %d\nWrite mode = %b\nStoreID = %d\nTraffic time = %d",
-                options.numberClients, options.writeMode, options.storeID, options.trafficTime));
+		@Override
+		public ThreadResult call() throws Exception {
+			return this.result;
+		}
+	}
 
-        TrafficGeneratorPerformanceMonitoring tgpm = new TrafficGeneratorPerformanceMonitoring();
-        try {
-            tgpm.runClients(options);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        } catch (NotBoundException e) {
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[] args) {
+		Options options = readArguments(args);
+		System.out.println(String.format("Number of clients: %d\nWrite mode = %b\nStoreID = %d\nTraffic time = %d",
+				options.numberClients, options.writeMode, options.storeID, options.trafficTime));
 
-    private void runClients(Options options) throws RemoteException, NotBoundException {
-        // ...
-        // todo inicializar X threads durante Y segundos (armar um alarme)
+		TrafficGeneratorPerformanceMonitoring tgpm = new TrafficGeneratorPerformanceMonitoring();
+		try {
+			tgpm.runClients(options);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			e.printStackTrace();
+		}
+	}
 
-        List<ClientThead> threads = new ArrayList<>();
-        Random random = new Random();
-        int randomClientID;
+	private void runClients(Options options) throws RemoteException, NotBoundException {
+		// ...
+		// todo inicializar X threads durante Y segundos (armar um alarme)
 
-        // initialize threads
-        for (int i = 0; i < options.numberClients; i++) {
-            randomClientID = random.nextInt();
-            threads.add(i, new ClientThead(options.writeMode, options.storeID, randomClientID));
-        }
+		List<ClientThead> threads = new ArrayList<>();
+		Random random = new Random();
+		int randomClientID;
 
-        // start the threads
-        for (ClientThead ct: threads) {
-            ct.start();
-        }
+		// initialize threads
+		for (int i = 0; i < options.numberClients; i++) {
+			randomClientID = random.nextInt();
+			threads.add(i, new ClientThead(options.writeMode, options.storeID, randomClientID));
+		}
 
-        // 1 sec clock for visual updates
-        Timer visualUpdateTimer = new Timer();
-        visualUpdateTimer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                elapsedTime++;
-                List<ThreadResult> updateResults = new ArrayList<>();
+		// start the threads
+		for (ClientThead ct : threads) {
+			ct.start();
+		}
 
-                for(ClientThead ct: threads) {
-                    try {
-                        updateResults.add(ct.getResult());
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
+		// 1 sec clock for visual updates
+		Timer visualUpdateTimer = new Timer();
+		visualUpdateTimer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				elapsedTime++;
+				List<ThreadResult> updateResults = new ArrayList<>();
 
-                printProgress(updateResults);
+				for (ClientThead ct : threads) {
+					try {
+						updateResults.add(ct.getResult());
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
 
-            }
-        }, 1000, 1000);
+				printProgress(updateResults);
 
-        List<ThreadResult> results = new ArrayList<>();
-        Timer trafficTimeTimer = new Timer();
-        trafficTimeTimer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                visualUpdateTimer.cancel();
-                stopThreads = true;
+			}
+		}, 1000, 1000);
 
-                for(ClientThead ct: threads) {
-                    try {
-                        results.add(ct.call());
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
+		List<ThreadResult> results = new ArrayList<>();
+		Timer trafficTimeTimer = new Timer();
+		trafficTimeTimer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				visualUpdateTimer.cancel();
+				stopThreads = true;
 
-                printFinalResults(results);
-            }
-        }, (options.trafficTime * 1000));
+				for (ClientThead ct : threads) {
+					try {
+						results.add(ct.call());
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
 
-    }
+				printFinalResults(results);
+			}
+		}, (options.trafficTime * 1000));
 
-    private void printProgress(List<ThreadResult> updateResults) {
-        // todo tratar e imprimir os resultados
-        double totalNumberRequests = 0, completedRequests = 0, unavailableRequests = 0;
-        long totalLatency = 0;
-        for (ThreadResult tr: updateResults) {
-            totalNumberRequests += tr.actionCount;
-            completedRequests += tr.replyCount;
-            unavailableRequests += tr.unavailableCount;
-            totalLatency += tr.timeSum;
-        }
+	}
 
-        double fulfilledRate = ((totalNumberRequests - unavailableRequests)/completedRequests);
-        System.out.println(String.format("Elapsed time: %d seconds\nThroughput: %.2f op/s\nAverage latency: %.2f ms\nFulfilled rate: %.2f%%",
-                elapsedTime, (completedRequests/elapsedTime), (totalLatency/totalNumberRequests), fulfilledRate));
+	private void printProgress(List<ThreadResult> updateResults) {
+		// todo tratar e imprimir os resultados
+		double totalNumberRequests = 0, completedRequests = 0, unavailableRequests = 0;
+		long totalLatency = 0;
+		for (ThreadResult tr : updateResults) {
+			totalNumberRequests += tr.actionCount;
+			completedRequests += tr.replyCount;
+			unavailableRequests += tr.unavailableCount;
+			totalLatency += tr.timeSum;
+		}
 
-    }
+		double fulfilledRate = ((totalNumberRequests - unavailableRequests) / completedRequests);
+		System.out.println(String.format(
+				"Elapsed time: %d seconds\nThroughput: %.2f op/s\nAverage latency: %.2f ms\nFulfilled rate: %.2f%%",
+				elapsedTime, (completedRequests / elapsedTime), (totalLatency / totalNumberRequests), fulfilledRate));
+		System.out.println("");
+		System.out.println("");
 
-    private void printFinalResults(List<ThreadResult> results) {
-        // todo tratar e imprimir os resultados
-        System.out.println("Acabou.");
-    }
+	}
 
-    /**
-     * Reads the arguments passed from the command line and creates a new Option object with the input parsed correctly.
-     * @param args cli args
-     * @return new Options object with the inputs parsed
-     */
-    private static Options readArguments(String[] args) {
-        Options result = new Options();
-        try {
-            result.numberClients = Integer.parseInt(args[0]);
-            result.trafficTime = Integer.parseInt(args[args.length - 1]);
-        } catch (java.lang.NumberFormatException e) {
-            System.err.println("Error while converting to integer. Did you run the program correctly?");
-            System.err.println("$ tgpm <c> [-w] [-s <sid>] <s>");
-            System.exit(0);
-        } catch ( ArrayIndexOutOfBoundsException e) {
-            System.err.println("Program requires at least TWO arguments, number of clients and active seconds.");
-            System.err.println("$ tgpm <c> [-w] [-s <sid>] <s>");
-            System.exit(0);
-        }
+	private void printFinalResults(List<ThreadResult> results) {
+		
 
-        String argsString = String.join(" ",args);
+		double totalNumberRequests = 0, completedRequests = 0, unavailableRequests = 0;
+		long totalLatency = 0;
+		for (ThreadResult tr : results) {
+			try {
+				tr.join();
+				System.out.println("o numero de threds ativas é: "+ ThreadResult.activeCount());
+				totalNumberRequests += tr.actionCount;
+				completedRequests += tr.replyCount;
+				unavailableRequests += tr.unavailableCount;
+				totalLatency += tr.timeSum;
+			} catch (InterruptedException e) {
 
-        if (argsString.contains("-w")) {
-            result.writeMode = true;
-        }
+				e.printStackTrace();
+			}
+		}
 
-        Pattern p = Pattern.compile("-s ([0-9]+)");
-        Matcher m = p.matcher(argsString);
-        if (m.find()) {
-            if (args.length > 3) {
-                result.storeID = Integer.parseInt(m.group(1));
-            } else {
-                System.err.println("When single store mode is enabled, the program requires AT LEAST 4 arguments.");
-                System.err.println("$ tgpm <c> [-w] [-s <sid>] <s>");
-                System.exit(0);
-            }
-        }
-        return result;
-    }
+		double fulfilledRate = ((totalNumberRequests - unavailableRequests) / completedRequests);
+		System.out.println("Number of requests sent: " + totalNumberRequests + "\nNumber of received replies: "
+				+ (completedRequests + unavailableRequests));
+		System.out.println(String.format(
+				"last one Elapsed time: %d seconds\nThroughput: %.2f op/s\nAverage latency: %.2f ms\nFulfilled rate: %.2f%%",
+				elapsedTime, (completedRequests / elapsedTime), (totalLatency / totalNumberRequests), fulfilledRate));
+	}
+
+	/**
+	 * Reads the arguments passed from the command line and creates a new Option
+	 * object with the input parsed correctly.
+	 * 
+	 * @param args
+	 *            cli args
+	 * @return new Options object with the inputs parsed
+	 */
+	private static Options readArguments(String[] args) {
+		Options result = new Options();
+		try {
+			result.numberClients = Integer.parseInt(args[0]);
+			result.trafficTime = Integer.parseInt(args[args.length - 1]);
+		} catch (java.lang.NumberFormatException e) {
+			System.err.println("Error while converting to integer. Did you run the program correctly?");
+			System.err.println("$ tgpm <c> [-w] [-s <sid>] <s>");
+			System.exit(0);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.err.println("Program requires at least TWO arguments, number of clients and active seconds.");
+			System.err.println("$ tgpm <c> [-w] [-s <sid>] <s>");
+			System.exit(0);
+		}
+
+		String argsString = String.join(" ", args);
+
+		if (argsString.contains("-w")) {
+			result.writeMode = true;
+		}
+
+		Pattern p = Pattern.compile("-s ([0-9]+)");
+		Matcher m = p.matcher(argsString);
+		if (m.find()) {
+			if (args.length > 3) {
+				result.storeID = Integer.parseInt(m.group(1));
+			} else {
+				System.err.println("When single store mode is enabled, the program requires AT LEAST 4 arguments.");
+				System.err.println("$ tgpm <c> [-w] [-s <sid>] <s>");
+				System.exit(0);
+			}
+		}
+		return result;
+	}
 }
