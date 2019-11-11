be0a3282b773923ee8abeb508a7027d2000a4b09
Frederico Apolónia <fredericojcapolo@gmail.com>
2019-11-11 23:44:49 +0000

    Modifiquei o client, o Manager estava a atrapalhar

--- src/main/java/ZooKeeper/ZooKeeperClientExample.java
+++ src/main/java/ZooKeeper/ZooKeeperClientExample.java
@@ -1,26 +1,75 @@
 package ZooKeeper;
 
-import org.apache.log4j.BasicConfigurator;
-import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
 
 import java.util.List;
 
 public class ZooKeeperClientExample {
-    private static ZooKeeperManager zooKeeperManager = new ZooKeeperManager("127.0.0.1");
+    private static ZooKeeperConnector zooKeeperManager;
+    private static ZooKeeper zooKeeper;
+    private static Watcher childrenChangedWatcher;
+    private static AsyncCallback.ChildrenCallback childrenCallBack;
 
     public static void main(String[] args) throws Exception {
+        zooKeeper = zooKeeperManager.connect("127.0.0.1");
+
         print("Strings sem fazer nada ao ZooKeeper");
-        printList(zooKeeperManager.getNodeChildren("/"));
+        Stat stat = zooKeeper.exists("/", false);
+        assert stat != null;
+
+        printList(zooKeeper.getChildren("/", null));
 
         print("Adicionar um novo no No1, com data xyz");
-        zooKeeperManager.create("/No1", "xyz".getBytes(), CreateMode.PERSISTENT);
-        printList(zooKeeperManager.getNodeChildren("/"));
+        zooKeeper.create("/No1", "xyz".getBytes(), ZooDefs.Ids.READ_ACL_UNSAFE, CreateMode.PERSISTENT);
 
-        String data = zooKeeperManager.getData("/No1", null);
-        print(String.format("Data do no criado: %s", data));
 
-        zooKeeperManager.deleteNode("/No1");
+        Stat no1Stat = zooKeeper.exists("/", false);
+        assert no1Stat != null;
+
+        byte[] b = zooKeeper.getData("/No1", false, null);
+        String result = new String(b);
+        print(String.format("Data do no criado: %s", result));
+
+        //childrenUpdateDemo();
+        //Thread.sleep(15000);
+
+        zooKeeper.delete("/No1", stat.getVersion());
+    }
+
+    public static void childrenUpdateDemo() throws Exception {
+        childrenChangedWatcher = watchedEvent -> {
+            if (watchedEvent.getType() == Watcher.Event.EventType.NodeChildrenChanged) {
+                try {
+                    printList(zooKeeper.getChildren(watchedEvent.getPath(), null));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        childrenCallBack = new AsyncCallback.ChildrenCallback() {
+            public void processResult(int rc, String path, Object ctx, List<String> children) {
+                switch (KeeperException.Code.get(rc)) {
+                    case CONNECTIONLOSS:
+                        getUpdatedChildren();
+
+                        break;
+                    case OK:
+                        print("Succesfully got a list of workers: "
+                                + children.size()
+                                + " workers");
+
+                        break;
+                    default:
+                        print("getChildren failed");
+                }
+            }
+        };
+        printList(zooKeeper.getChildren("/", childrenChangedWatcher));
+    }
+
+    public static void getUpdatedChildren() {
+        zooKeeper.getChildren("/No1", childrenChangedWatcher, childrenCallBack, null);
     }
 
     public static void print(String s) {
