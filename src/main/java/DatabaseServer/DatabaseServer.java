8f9fe2383e5990f6d4216ded97cb8768bb24b159
Frederico Apolónia <fredericojcapolo@gmail.com>
2019-11-13 23:36:34 +0000

    BD agora liga-se e regista-se no zookeeper

--- src/main/java/DatabaseServer/DatabaseServer.java
+++ src/main/java/DatabaseServer/DatabaseServer.java
@@ -1,31 +1,57 @@
 package DatabaseServer;
 
 import DatabaseServer.Interfaces.IDataBase;
+import ZooKeeper.ZooKeeperConnector;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 // import zookeeper classes
-import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.*;
 import org.apache.zookeeper.Watcher.Event.KeeperState;
-import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.AsyncCallback.StatCallback;
 import org.apache.zookeeper.KeeperException.Code;
 import org.apache.zookeeper.data.Stat;
 
 public class DatabaseServer {
 
-    private static final int DATABASE_PORT = 1100;
+    private static final String FILE_SEPARATOR = File.separator;
+    // property fetches the home path
+    private static final String ZK_PATH = System.getProperty("user.home")
+            + FILE_SEPARATOR + "AllstoresDB" + FILE_SEPARATOR;
+
+    private static ZooKeeperConnector zooKeeperConnector;
+
 
     public static void main(String[] args) throws Exception {
+        // connect and register in zookeeper
+        String zooKeeperHost = getZooKeeperHost();
+        ZooKeeper zooKeeper = zooKeeperConnector.connect(zooKeeperHost);
+
+        Stat appStat = zooKeeper.exists("/db", false);
+        if (appStat == null) {
+            zooKeeper.create("/db", "15500".getBytes(), ZooDefs.Ids.READ_ACL_UNSAFE, CreateMode.PERSISTENT);
+            appStat = zooKeeper.exists("/db", false);
+        }
+
+        String znodePath = zooKeeper.create("/db/", "".getBytes(), ZooDefs.Ids.READ_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        String[] znodePathSplit = znodePath.split("/");
+        int sequentialNumber = Integer.parseInt(znodePathSplit[znodePathSplit.length - 1].replace("0",""));
+        int port = Integer.parseInt(new String(zooKeeper.getData("/db", false, appStat))) + sequentialNumber;
+
+        // port must be higher than 15500 so it doesn't collide with AllStoresApp
+        assert port > 15500;
+
         IDataBase database = new DatabaseImpl();
 
         /* Create registry and rebind it to port DATABASE_PORT */
         Registry registry = null;
         try {
-            registry = LocateRegistry.createRegistry(DATABASE_PORT);
+            registry = LocateRegistry.createRegistry(port);
             registry.rebind("AllstoresDatabaseServer", database);
         } catch (Exception e) {
             System.err.println("DataBase: ERROR trying to start server");
@@ -46,6 +72,13 @@ public class DatabaseServer {
 
         String myID = address + ":" + "AllstoresDatabaseServer";
         System.out.println(myID);
+    }
+
+    private static String getZooKeeperHost() throws IOException {
+        BufferedReader fileReader = new BufferedReader(new FileReader(ZK_PATH + "conf.cfg"));
+        String result = fileReader.readLine();
+        fileReader.close();
+        return result;
     }
 
 }
